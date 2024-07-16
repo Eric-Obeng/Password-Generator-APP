22fa6346f9b3a8c2764c8889ac3ce83dadc68391
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-16 04:25:45 +0000

    chore: Update options component with new input fields and event emitter

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-options',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './options.component.html',
-  styleUrl: './options.component.css'
+  styleUrl: './options.component.css',
 })
 export class OptionsComponent {
+  includeUpperCase: boolean = true;
+  includeLowerCase: boolean = true;
+  includeNumbers: boolean = true;
+  includeSymbols: boolean = false;
 
+  @Output() optionsChange = new EventEmitter<any>();
+
+  onOptionChange() {
+    this.optionsChange.emit({
+      includeUpperCase: this.includeUpperCase,
+      includeLowerCase: this.includeLowerCase,
+      includeNumbers: this.includeNumbers,
+      includeSymbols: this.includeSymbols,
+    });
+  }
 }
