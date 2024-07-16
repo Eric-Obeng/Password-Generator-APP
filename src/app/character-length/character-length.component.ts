d87237c24cb03a1934e5bf06cf3906c6c0da277c
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-16 04:24:22 +0000

    characters length logic updated

--- src/app/character-length/character-length.component.ts
+++ src/app/character-length/character-length.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-character-length',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './character-length.component.html',
-  styleUrl: './character-length.component.css'
+  styleUrl: './character-length.component.css',
 })
 export class CharacterLengthComponent {
+  length: number = 10;
 
+  @Output() lengthChange = new EventEmitter<number>();
+
+  onLengthChange() {
+    this.lengthChange.emit(this.length);
+  }
 }
